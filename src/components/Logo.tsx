81d3596a5200bba0bd19c8b37068d9cb4753be79
Terrance <gibson.terrance.bs@outlook.com>
2025-07-13 02:04:25 -0400

    fix: logo import and metatags

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
+import logo from '../assets/logo.png';
+
 export function Logo({ className, logoClassName, textClassName, showText = true }: { className?: string; logoClassName?: string; textClassName?: string; showText?: boolean }) {
   return (
     <section id="logo-container" className={`${className}`}>
-      <img id="logo-img" src="src/assets/logo.png" alt="logo" draggable={false} className={`${logoClassName} mx-auto w-36`} />
+      <img id="logo-img" src={logo} alt="logo" draggable={false} className={`${logoClassName} mx-auto w-36`} />
 
       {showText && (
         <p id="logo-text" className={`${textClassName} select-none font-hypatia font-sem -mt-2 text-center tracking-widest uppercase`}>
